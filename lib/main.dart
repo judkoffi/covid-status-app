a1b4f9f28b5f87998659e54b2492d4f5ff5e9bec
judi <judkoffi@gmail.com>
2020-09-11 23:28:08 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:covid_info_app/views/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
---
+++ lib/services/covid_api.dart
@@ -0,0 +1,7 @@
+import 'package:covid_info_app/models/country.dart';
+
+class CovidAPIService {
+  static const API_BASE_URL = 'https://api.covid19api.com';
+
+  List<Country> getCountries() {}
+}

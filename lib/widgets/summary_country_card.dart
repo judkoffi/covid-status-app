2b9b90a16fbd47f2e703824e50fa9792d6159809
judi <judkoffi@gmail.com>
2020-09-12 18:14:47 +0200

    release apk

---
+++ lib/widgets/summary_country_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class SummaryCountryCard extends StatefulWidget {
+  @override
+  _SummaryCountryCardState createState() => _SummaryCountryCardState();
+}
+
+class _SummaryCountryCardState extends State<SummaryCountryCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
